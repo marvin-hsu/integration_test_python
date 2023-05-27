1b14399007c67636c925e8a3bb485b369f7b4f41
Marvin Hsu <marv.hsu@icloud.com>
2023-05-27 06:57:56 +0000

    add test.yml

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
-@pytest.fixture(scope="session")
-def config(request):
-    env = request.config.getoption("--env")
-    config = Config(env)
-    yield config
+# @pytest.fixture(scope="session")
+# def config(request):
+#     env = request.config.getoption("--env")
+#     config = Config(env)
+#     yield config
 
 def pytest_addoption(parser):
     parser.addoption("--env", action="store", default="local", help="set environment")
