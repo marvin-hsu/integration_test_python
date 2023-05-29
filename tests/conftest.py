38870e4a584bcc6d69001d2717658904542625ed
Marvin Hsu <marvinhsu@nine-yi.com>
2023-05-29 13:28:32 +0800

    本機環境下需先啟動專案程式

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import subprocess
+
 import pytest
 
 from tests.utils.config_reader import Config
@@ -11,6 +13,24 @@ def config(request):
     config = Config(env)
     # 返回config物件
     yield config
+
+
+@pytest.fixture(scope="session", autouse=True)
+def project(request):
+    env = request.config.getoption("--env")
+    if env == "local":
+
+        # 在測試開始前啟動src中專案
+        cmd = ["python", "-u", "-m", "src.demo"]
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+
+        # 等待 process 打印 "Hello World!
+        while True:
+            line = process.stdout.readline()
+            if line.strip() == "Hello World!":
+                break
+
+    yield
 
 
 def pytest_addoption(parser):
