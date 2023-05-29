c5ad8571bc1afc13d36cc7cafc859c00592e6052
Marvin Hsu <marvinhsu@nine-yi.com>
2023-05-29 10:22:26 +0800

    update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,17 @@
-# @pytest.fixture(scope="session")
-# def config(request):
-#     env = request.config.getoption("--env")
-#     config = Config(env)
-#     yield config
+import pytest
+
+from tests.utils.config_reader import Config
+
+
+@pytest.fixture(scope="session")
+def config(request):
+    # 從命令行參數中獲取env值
+    env = request.config.getoption("--env")
+    # 創建config物件
+    config = Config(env)
+    # 返回config物件
+    yield config
+
 
 def pytest_addoption(parser):
     parser.addoption("--env", action="store", default="local", help="set environment")
--- tests/utils/config_reader.py
+++ tests/utils/config_reader.py
@@ -1,22 +1,31 @@
+import os
+
+import toml
+
+
 class Config:
     def __init__(self, env):
+        self.config = None
         self.env = env
         self.load_config()
 
     def load_config(self):
+        config_directory = os.path.join(os.path.dirname(os.getcwd()), "configurations")
+        config_path = None
+
         if self.env == "qa":
-            # 讀取qa.toml並序列化為config物件
-            with open("qa.toml", "r") as f:
-                config_data = toml.load(f)
-            # 執行設定檔初始化
-            # ...
+            config_path = os.path.join(config_directory, "qa.toml")
 
         elif self.env == "local":
-            # 讀取local.toml並序列化為config物件
-            with open("local.toml", "r") as f:
-                config_data = toml.load(f)
-            # 執行設定檔初始化
-            # ...
+            config_path = os.path.join(config_directory, "local.toml")
+
+        with open(config_path, "r") as f:
+            config_data = toml.load(f)
 
         # 將config_data轉換為config物件
         self.config = ConfigObject(config_data)
+
+
+class ConfigObject:
+    def __init__(self, config_data):
+        self
